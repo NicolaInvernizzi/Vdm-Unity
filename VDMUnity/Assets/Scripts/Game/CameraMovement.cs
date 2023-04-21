78b714fd89ed528b97182c6474e995c56d7abe1e
NicolaInvernizzi <nicola.invernizzi99@gmail.com>
2023-04-21 17:49:52 +0200

    Camera fix
    Remove Time.DeltaTime in camera movement calculation

--- VDMUnity/Assets/Scripts/Game/CameraMovement.cs
+++ VDMUnity/Assets/Scripts/Game/CameraMovement.cs
@@ -4,7 +4,7 @@ public class CameraMovement : MonoBehaviour
 {
     public SaveSystem saveSystem;
     public Transform player;
-    [SerializeField, Range(10f, 200f)] private float cameraSensibility;
+    [SerializeField, Range(0f, 1f)] private float cameraSensibility;
     [SerializeField, Range(10f, 90f)] private float motionRange_UPDown;
 
     private Data data;
@@ -14,23 +14,8 @@ public class CameraMovement : MonoBehaviour
     {
         data = GetComponent<Data>();
     }
-    //private void Start()
-    //{
-    //    playerRotationY = 0;
-    //    cameraRotationX = 0;
-    //}
-
     void Update()
     {
-        //if (saveSystem.playerRotation)
-        //{
-        //    player.localRotation = Quaternion.Euler(0f, data.rotation.y, 0f);
-        //    Debug.Log(data.rotation.y);
-        //    transform.localRotation = Quaternion.Euler(data.rotation.x, 0f, 0f);
-        //    Debug.Log(data.rotation.x);
-        //    saveSystem.playerRotation = false;
-        //}
-
         if (saveSystem.playerRotation)
         {
             playerRotationY = data.rotation.y;
@@ -42,8 +27,8 @@ public class CameraMovement : MonoBehaviour
 
         if (Cursor.lockState == CursorLockMode.Locked)
         {
-            playerRotationY += Input.GetAxis("Mouse X") * cameraSensibility * Time.deltaTime;
-            cameraRotationX -= Input.GetAxis("Mouse Y") * cameraSensibility * Time.deltaTime;
+            playerRotationY += Input.GetAxis("Mouse X") * cameraSensibility;
+            cameraRotationX -= Input.GetAxis("Mouse Y") * cameraSensibility;
             //cameraRotationX = Mathf.Clamp(cameraRotationX, -motionRange_UPDown, motionRange_UPDown);
             player.localRotation = Quaternion.Euler(0f, playerRotationY, 0f);
             transform.localRotation = Quaternion.Euler(cameraRotationX, 0f, 0f);
